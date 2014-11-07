415ed9d7de1dc0f2e5bebf3ed6ed28dbf12cfe5a
Luke Jefferson <luke.jefferson@gmail.com>
2014-11-07 17:13:19 -0500

    2.2 FizzBuzz

--- chapter2.js
+++ chapter2.js
@@ -3,6 +3,8 @@
   Chapter 2 - Program Structure
 */
 
+// 2.1 LOOPING A TRIANGLE
+
 function triangle1(n) {
   var line = "",
       i = 1;
@@ -22,3 +24,21 @@ function triangle2(n) {
   }
 }
 triangle2(7);
+
+// 2.2 FIZZBUZZ
+
+function fizzBuzz(n) {
+  var i = 1;
+
+  for (i; i <= n; i++) {
+    var output = "";
+    if (i % 3 == 0) {
+      output += "Fizz";
+    }
+    if (i % 5 == 0) {
+      output += "Buzz";
+    }
+    console.log(output || i);
+  }
+}
+fizzBuzz(100);
