e7be6b0d70f0b9a6744d7879b9cd8548c8577b2c
Luke Jefferson <luke.jefferson@gmail.com>
2014-11-07 18:00:52 -0500

    3.2 Recursion

--- chapter2.js
+++ chapter2.js
@@ -72,3 +72,20 @@ function min(a, b) {
 }
 console.log(min(1,2));
 
+// 3.2 RECURSION
+function isEven(number) {
+  if (number == 0)
+    return true;
+  else if (number == 1)
+    return false;
+  else if (number < 0)
+    return isEven(-number);
+  else
+    return isEven(number - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(isEven(-10));
+
